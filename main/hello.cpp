9f4bd9aa21ebca927d273d31a7bbfea2a2c48416
nandinibs-2003 <116246816+nandinibs-2003@users.noreply.github.com>
2024-03-10 16:56:46 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, Worldsss\n";
-  cout << "Hello, Je\n";
+  cout << "Hello, Jeki\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
