381b74a3670ee7ec14e6025106d15ae28e2ea8b1
nandinibs-2003 <116246816+nandinibs-2003@users.noreply.github.com>
2024-03-11 12:49:47 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,10 @@
-//File: hello.cpp
 #include <iostream>
 using namespace std;
 
 int main(void) {
-  cout << "Hello, Worldsss\n";
-  cout << "Hello, Jekinbnss\n";
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "Webhook successfully enabled\n"; 
   return 0;
 }
